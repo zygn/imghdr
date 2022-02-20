44b7b13eea8ecb1827041f6cc13c725e5c427536
YundoChoi <rikochet@kakao.com>
2022-02-20 16:56:32 +0900

    fixed path testset

--- example/test.rb
+++ example/test.rb
@@ -1,7 +1,7 @@
 
 require "imghdr"
 
-file_list = Dir.glob("*.{jpg,jpeg,png,gif,im32,pbm,pgm,ppm,xbm,xpm,tiff,tif,rgb}")
+file_list = Dir.glob("testset/*.{jpg,jpeg,png,gif,im32,pbm,pgm,ppm,xbm,xpm,tiff,tif,rgb}")
 
 file_list.each do |file|
   puts "File: #{file}"
