d21a99522279471c21a401ec30e7c608a796741d
YundoChoi <rikochet@kakao.com>
2022-02-22 01:35:42 +0900

    Update testcases (EXR, RASTER, WEBP, XBM)

--- example/test.rb
+++ example/test.rb
@@ -1,7 +1,6 @@
-
 require "imghdr"
 
-file_list = Dir.glob("testset/*.{jpg,jpeg,png,gif,im32,pbm,pgm,ppm,xbm,xpm,tiff,tif,rgb}")
+file_list = Dir.glob("testset/*.{jpg,jpeg,png,gif,im32,pbm,pgm,ppm,xbm,xpm,tiff,tif,rgb,webp,exr,ras}")
 
 file_list.each do |file|
   puts "File: #{file}"
